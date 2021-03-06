108e483b65edae17426d661c277d5e5b4fddd596
Felipe Sulczinski <matos.szk@gmail.com>
2021-03-05 23:08:50 -0300

    feat(app): app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsListComponent } from './cards-list/cards-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cards-list' },
+  { path: 'cards-list', pathMatch: 'full', component: CardsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsListModule } from './cards-list/cards-list.module';
+import { HeaderComponent } from './shared/component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
